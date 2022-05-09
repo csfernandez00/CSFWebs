16765b8c87c676b7d0f7718f66dffe0ae026fac6
csfernandez00 <csfernandez00@gmail.com>
2022-05-09 19:15:09 -0300

    Actualizacion services

--- codigo.js
+++ codigo.js
@@ -20,71 +20,73 @@ $(function(){
         $("#btn-advanc").removeClass("selected");
         $(".pack").html(`
         <h2>Paquete <span>Basico</span></h2>
-        <p>Ideal para aquellas empresas que acaban de empezar y quieren crecer rápidamente. Darle más vista a tu negocio nunca fue tan <b>económico</b>. </p>
+        <p>Ideal para aquellas empresas que acaban de empezar y quieren crecer rápidamente. Elegí una de nuestras plantillas y obtene tu sitio en tan solo 5 días.</p>
         <h3>Incluye:</h3>
         <div class="includes">
             <ul>
-                <li>Hasta 3 secciones</li>
-                <li>Secciones a elección </li>
-                <li>Posibles revisiones:  1  </li>
-                <li>Entrega: 2 a 5 días <i>(hábiles)</i> </li>
+                <li>Carga de información </li>
+                <li>Infinitas revisiones</li>
+                <li>Entrega: 5 días hábiles </li>
+                <li>Integración redes sociales</li>
             </ul>
             <ul>
-                <li><span class="adv">Formato de secciones modificable</span></li>
-                <li><span class="adv">Integración de Maps</span></li>
+                
+                <li>Integración de Maps</li>
+                <li>Hosting Privado</li>
                 <li><span class="adv">Carrito y Buscador</span></li>
                 <li><span class="adv">Pasarelas de Pago (MercadoPago)</span></li>
             </ul>
         </div>`);
-        $(".precio").html("45 a 80");
+        $(".precio").html("$1500");
     });
     $("#btn-interm").click(()=>{
         $("#btn-interm").addClass("selected");
         $("#btn-basic").removeClass("selected");
         $("#btn-advanc").removeClass("selected");
         $(".pack").html( `
-        <h2>Paquete <span>Intermedio</span></h2>
-        <p> Estas preparado para pasar por arriba a la competencia? Modificamos las secciones a tu gusto y además podrás mostrar tu ubicación en <b>Maps</b>.</p>
+        <h2>Paquete <span>Personalizado</span></h2>
+        <p> Tenes una idea para tu sitio web y no sabes como armarlo? Nosotros te lo creamos totalmente personalizado a tu manera.</p>
         <h3>Incluye:</h3>
         <div class="includes">
             <ul>
-                <li>Hasta 5 secciones</li>
                 <li>Secciones a elección  </li>
-                <li>Posibles revisiones:  3  </li>
-                <li>Entrega: 4 a 8 días <i>(hábiles)</i> </li>
+                <li>Infinitas revisiones</li>
+                <li>Entrega: 5 días hábiles </li>
             </ul>
             <ul>
-                <li>Formato de secciones modificable</li>
+                <li>Integración redes sociales</li>
                 <li>Integración de Maps</li>
+                <li>Hosting Privado</li>
                 <li><span class="adv">Carrito y Buscador</span></li>
                 <li><span class="adv">Pasarelas de Pago (MercadoPago)</span></li>
             </ul>
         </div>`);
-        $(".precio").html("80 a 150");
+        $(".precio").html("$2000");
     });
     $("#btn-advanc").click(()=>{
         $("#btn-advanc").addClass("selected");
         $("#btn-interm").removeClass("selected");
         $("#btn-basic").removeClass("selected");
         $(".pack").html( `
-        <h2><span>E-Commerce</span></h2>
+        <h2>Modelo <span>E-Commerce</span></h2>
         <p> Estas dispuesto a llevar tu negocio al siguiente nivel? Empezá a ofrecer tus productos y/o servicio a traves de tu nuevo sitio web de modelo <b>E-Commerce</b>.</p>
         <h3>Incluye:</h3>
         <div class="includes">
             <ul>
                 <li>Secciones a elección </li>
-                <li>Posibles revisiones:  5  </li>
+                <li>Infinitas revisiones </li>
                 <li>Entrega: 5 a 10 días <i>(hábiles)</i> </li>
-                <li>Formato de secciones modificable</li>
+                <li>Integración redes sociales</li>
             </ul>
             <ul>
                 <li>Integración de Maps</li>
+                <li>Hosting Privado</li>
                 <li>Carrito y Buscador</li>
                 <li>Pasarelas de Pago (MercadoPago)</li>
                 
             </ul>
         </div>`);
-        $(".precio").html( "150 a 250");
+        $(".precio").html( "$2500");
     });
 });
 
